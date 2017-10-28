c4413cec239a1d40dd2ebd44dd5e10b595ba375c
FruitWave <league@WTS8.attlocal.net>
2017-10-27 18:23:00 -0700

    secretMessageBox , Badgers , If / Else Checkpoint

---
+++ src/Badgers.java
@@ -0,0 +1,21 @@
+
+public class Badgers {
+public static void main(String[] args) {
+	String b = "badgers";
+	String m = "mushroom";
+	for (int i = 0; i < 3; i++) {
+		
+	
+	for (int x = 0; x < 10; x++) {
+	System.out.println(b);	
+	}
+	
+	for (int y = 0; y < 2; y++) {
+		System.out.println(m);
+	}
+	}
+	for (int z = 0; z < 2; z++) {
+		System.out.println("SNAKE");
+	}
+}
+}
--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -16,17 +16,27 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		
 	
 	// 1. Set the passcode in a String variable
-	
+	String password = "Biggie cheese";
 	// 2. Using a pop-up, ask for a secret message and store it in a variable
-
+String sm = JOptionPane.showInputDialog("What is the secret message?");
 	// 3. Ask your friend for the passcode and store it in a variable
-
+String passcode = JOptionPane.showInputDialog("What is the passcode?");
 	// 4. If the passcode matches, show the secret message
-
+if (passcode == password) {
+	JOptionPane.showMessageDialog(null, sm);
+}
+else if (passcode.equalsIgnoreCase("Biggie cheese")) {
+	JOptionPane.showMessageDialog(null, sm);
+}
 	// 5. If the passcode does not match, pop-up "INTRUDER!!"
 
+else {
+	JOptionPane.showMessageDialog(null, "INTRUDER!");
+}
 	// [optional] 6. Have your friend also enter a username, and make sure it is correct before releasing the secret message.
-
+	}
 }
